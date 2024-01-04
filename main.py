740a58c62325f9a4da028428812d23da69537b75
Akintayo Akinpelu <Akinpeluakintayo@outlook.com>
2024-01-04 00:13:03 +0000

    Data Ingestion

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from src import logger
+from src.pipeline.data_ingestion import DataIngestionTrainingPipeline
 
 
-logger.info("This is my first log!")
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
